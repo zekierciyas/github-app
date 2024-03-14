735b34562d3aae1f13285fea58a5f9d0f834e882
zekierciyas <71823127+zekierciyas@users.noreply.github.com>
2024-03-14 21:28:22 +0300

    chore: overall project&dependency setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # github-app
- An android app that lists users on Github and shows the user informations in detail page
+ An android app that lists users on Github and shows the user information's in detail page
+
+# Conventional Commit Guidelines
+
+In order to explain the commit history in the project more clearly, the commit guideline was designed according to European conventional commit rules. General commit types are as follows, check here for more detailed information. General commit types are as follows, check [here](https://ec.europa.eu/component-library/v1.15.0/eu/docs/conventions/git/) for more detailed information.
+
+* **feat**: A new feature
+* **fix**: A bug fix
+* **docs**: Documentation only changes
+* **style**: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+* **refactor**: A code change that neither fixes a bug nor adds a feature
+* **perf**: A code change that improves performance
+* **test**: Adding missing tests
+* **chore**: Changes to the build process or auxiliary tools and libraries such as documentation generation
---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.zekierciyas.github_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.zekierciyas.github_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    //Core, Androidx
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    //Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Hilt, Dependency Injection
+    implementation(libs.hilt)
+    kapt(libs.hiltCompiler)
+
+    //Navigation Component
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.testing)
+
+    //Room, Caching
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+
+    //Retrofit, Networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.mock)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+
+    //Coroutine, Lightweight Threading
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    //Glide, Image Loading
+    implementation(libs.glide.lib)
+
+}
